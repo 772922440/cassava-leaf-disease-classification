3752879c18cb83fdd887e5047bdf42abd99f1758
hujian <janhu9527@gmail.com>
2021-01-26 12:50:10 +0800

    先干掉 oneof

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -38,39 +38,37 @@ def get_albu_transform(image_size):
                 A.Resize(512,512),
                 A.RandomRotate90(),
                 A.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225],
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
                 ),
                 A.Flip(),
                 A.RandomCrop(width=512, height=512),
                 A.HorizontalFlip(p=0.5),
                 A.RandomBrightnessContrast(p=0.2),
-                A.OneOf([
-                    A.IAAAdditiveGaussianNoise(),
-                    A.GaussNoise(),
-                    ], p=0.2),
-                A.OneOf([
-                    A.MotionBlur(p=.2),
-                    A.MedianBlur(blur_limit=3, p=0.1),
-                    A.Blur(blur_limit=3, p=0.1),
-                    ], p=0.2),
+                # A.OneOf([
+                #     A.IAAAdditiveGaussianNoise(),
+                #     A.GaussNoise(),
+                #     ], p=0.2),
+                # A.OneOf([
+                #     A.MotionBlur(p=.2),
+                #     A.MedianBlur(blur_limit=3, p=0.1),
+                #     A.Blur(blur_limit=3, p=0.1),
+                #     ], p=0.2),
                 A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
-                A.OneOf([
-                    A.OpticalDistortion(p=0.3),
-                    A.GridDistortion(p=.1),
-                    A.IAAPiecewiseAffine(p=0.3),
-                    ], p=0.2),
-
-                A.OneOf([
-                #     A.CLAHE(clip_limit=2),
-                    A.IAASharpen(),
-                    A.IAAEmboss(),
-                    A.RandomBrightnessContrast(),            
-                    ], p=0.3),
+                # A.OneOf([
+                #     A.OpticalDistortion(p=0.3),
+                #     A.GridDistortion(p=.1),
+                #     A.IAAPiecewiseAffine(p=0.3),
+                #     ], p=0.2),
+                # A.OneOf([
+                #     A.IAASharpen(),
+                #     A.IAAEmboss(),
+                #     A.RandomBrightnessContrast(),            
+                #     ], p=0.3),
                 A.HueSaturationValue(p=0.3),
                 ToTensorV2(),
                 ])
-                
+
     test_trans = A.Compose([
         A.Resize(512,512),
         A.Normalize(
