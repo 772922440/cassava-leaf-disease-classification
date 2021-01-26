9cda3fe49b4d0ea211563406d5b4fb133333ef5a
hujian <janhu9527@gmail.com>
2021-01-26 09:05:32 +0800

    remove models

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,12 +1,9 @@
 from .resnet import *
 
+# mobilenetv3_large_100
+# resnet18
+# resnext50_32x4d
+# tf_efficientnet_b3
 def get_backbone(backbone, config):
-    if backbone=='resnet18':
-        backbone = CustomResNext18(target_size=config.target_size)
-    elif backbone=='resnext50_32x4d':
-        backbone = CustomResNext50(target_size=config.target_size)
-    elif backbone=='mobilenetv3_large_100':
-        backbone = MobileNetV3(target_size=config.target_size)
-    else:
-        backbone = MobileNetV3(model_name=backbone, target_size=config.target_size)
+    backbone = TimmBackbone(model_name=backbone, target_size=config.target_size)
     return backbone
