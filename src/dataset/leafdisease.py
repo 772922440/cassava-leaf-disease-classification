a9e66430864cc57371095faf436fdee1725816a8
hujian <janhu9527@gmail.com>
2021-01-27 13:42:55 +0800

    fix

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -70,26 +70,13 @@ def get_albu_transform(transform, config):
                     A.RandomBrightnessContrast(p=0.2),
                     A.OneOf([
                         A.IAAAdditiveGaussianNoise(),
-#                        A.GaussNoise(),
                         ], p=0.2),
-#                    A.OneOf([
-#                        A.MotionBlur(p=.2),
-#                        A.MedianBlur(blur_limit=3, p=0.1),
-#                        A.Blur(blur_limit=3, p=0.1),
-#                        ], p=0.2),
                     A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
-#                    A.OneOf([
-#                        A.OpticalDistortion(p=0.3),
-#                        A.GridDistortion(p=.1),
-#                        A.IAAPiecewiseAffine(p=0.3),
-#                        ], p=0.2),
                     A.OneOf([
                         A.CLAHE(clip_limit=2),
                         A.IAASharpen(),
-#                       A.IAAEmboss(),
                         A.RandomBrightnessContrast(),            
                         ], p=0.3),
-#                    A.HueSaturationValue(p=0.3),
                     ], p=config.p),
                 A.Resize(config.image_size,config.image_size),
                 A.Normalize(
