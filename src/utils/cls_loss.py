c5b1ddad405cba7df006af8f471126585b86d42a
hujian <janhu9527@gmail.com>
2021-01-27 16:00:26 +0800

    fix focal cosine

--- src/utils/cls_loss.py
+++ src/utils/cls_loss.py
@@ -37,6 +37,24 @@ class LabelSmoothingLoss(nn.Module):
             true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence) 
         return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
 
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        BCE_loss = nn.CrossEntropyLoss()(inputs, targets)
+
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
+
 class FocalCosineLoss(nn.Module):
     def __init__(self, alpha=1, gamma=2, xent=.1):
         super(FocalCosineLoss, self).__init__()
@@ -50,29 +68,6 @@ class FocalCosineLoss(nn.Module):
             F.normalize(input), F.one_hot(target, num_classes=input.size(-1)), self.y, reduction=reduction)
         cent_loss = F.cross_entropy(input, target, reduce=False)
 
-        pt = torch.exp(-cent_loss)
-        focal_loss = self.alpha * (1-pt)**self.gamma * cent_loss
-
-        if reduction == "mean":
-            focal_loss = torch.mean(focal_loss)
-
-        return cosine_loss + self.xent * focal_loss
-
-
-class FocalCosineLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2, xent=.1):
-        super(FocalCosineLoss, self).__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-
-        self.xent = xent
-
-        self.y = torch.Tensor([1]).cuda()
-
-    def forward(self, input, target, reduction="mean"):
-        cosine_loss = F.cosine_embedding_loss(input, F.one_hot(target, num_classes=input.size(-1)), self.y, reduction=reduction)
-
-        cent_loss = F.cross_entropy(F.normalize(input), target, reduce=False)
         pt = torch.exp(-cent_loss)
         focal_loss = self.alpha * (1-pt)**self.gamma * cent_loss
 
