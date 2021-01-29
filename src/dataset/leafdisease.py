e9b9f45813b39bd4303ccc3bd582fe60f4051ce2
Akhim-yun <56524864+Akhim-yun@users.noreply.github.com>
2021-01-29 14:34:24 +0800

    Update leafdisease.py

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -30,16 +30,13 @@ def get_albu_transform(transform, config):
                     A.RandomCrop(width=config.image_size, height=config.image_size),
                     A.HorizontalFlip(p=0.5),
                     A.RandomBrightnessContrast(p=0.2),
-                    A.OneOf([
-                        A.IAAAdditiveGaussianNoise(),
-                       A.GaussNoise(),
-                        ], p=0.2),
+                    
                    A.OneOf([
                        A.MotionBlur(p=.2),
                        A.MedianBlur(blur_limit=3, p=0.1),
                        A.Blur(blur_limit=3, p=0.1),
                        ], p=0.2),
-                    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
+            
                    A.OneOf([
                        A.OpticalDistortion(p=0.3),
                        A.GridDistortion(p=.1),
