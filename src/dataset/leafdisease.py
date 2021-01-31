2d7695c92de55d97b929aae5533ef2dbdc3a7af4
hijkzzz <janhu9527@gmail.com>
2021-01-31 15:17:33 +0000

    update

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -90,20 +90,20 @@ def get_albu_transform(transform, config):
     elif transform == "strong_fix2":
         train_trans =  A.Compose([
                 # 旋转平移
-                A.RandomRotate90(p=0.5),
-                A.Flip(p=0.5),
-                A.HorizontalFlip(p=0.5),
+                A.RandomRotate90(p=0.4),
+                A.Flip(p=0.4),
+                A.HorizontalFlip(p=0.4),
                 A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.3),
 
                 # 光照色彩
-                A.RandomSunFlare(num_flare_circles_lower=1, num_flare_circles_upper=3, src_radius=120, p=0.3),
+                A.RandomSunFlare(num_flare_circles_lower=1, num_flare_circles_upper=2, src_radius=200, p=0.3),
                 A.RandomBrightnessContrast(p=0.2),
                 A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, p=0.4),
                 
                 # Noise/扭曲/Mask
                 A.ISONoise(p=0.2),
-                A.OpticalDistortion(p=0.2),
-                A.Cutout(num_holes=4, max_h_size=90, max_w_size=120, fill_value=0, always_apply=False, p=0.2),
+                A.OpticalDistortion(p=0.3),
+                A.Cutout(num_holes=4, max_h_size=90, max_w_size=120, fill_value=0, always_apply=False, p=0.3),
 
                 # 归一化
                 A.Resize(config.image_size,config.image_size),
