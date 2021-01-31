8b2a0cf78f6b66458878f53c1eb7bfd9777ac0b7
hijkzzz <janhu9527@gmail.com>
2021-01-31 13:03:26 +0000

    add crop

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -30,7 +30,6 @@ def get_albu_transform(transform, config):
             ToTensorV2(),
         ])
     else:
-
         test_trans = A.Compose([
             A.Resize(config.image_size,config.image_size),
             A.Normalize(
@@ -102,20 +101,23 @@ def get_albu_transform(transform, config):
                 ])
     elif transform == "strong_fix2":
         train_trans =  A.Compose([
+                # 随机裁剪
+                A.RandomCrop(width=400, height=300, p=0.3),
+
                 # 旋转平移
                 A.RandomRotate90(p=0.5),
                 A.Flip(p=0.5),
                 A.HorizontalFlip(p=0.5),
-                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.5),
+                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
 
                 # 光照色彩
-                A.RandomBrightnessContrast(p=0.5),
+                A.RandomBrightnessContrast(p=0.2),
                 A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, p=0.5),
                 
                 # Noise/扭曲/Mask
-                A.ISONoise(p=0.5),
-                A.OpticalDistortion(p=0.5),
-                A.Cutout(num_holes=8, max_h_size=60, max_w_size=60, fill_value=0, always_apply=False, p=0.5),
+                A.ISONoise(p=0.2),
+                A.OpticalDistortion(p=0.2),
+                A.Cutout(num_holes=4, max_h_size=60, max_w_size=60, fill_value=0, always_apply=False, p=0.3),
 
                 # 归一化
                 A.Resize(config.image_size,config.image_size),
