01490fa8c53b9488a14810789c3fbc22054601b5
hijkzzz <janhu9527@gmail.com>
2021-02-01 01:19:15 +0000

    update

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -93,7 +93,7 @@ def get_albu_transform(transform, config):
                 A.RandomRotate90(p=0.4),
                 A.Flip(p=0.4),
                 A.HorizontalFlip(p=0.4),
-                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.3),
+                A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.4),
 
                 # 光照色彩
                 A.RandomSunFlare(num_flare_circles_lower=1, num_flare_circles_upper=2, src_radius=200, p=0.3),
@@ -102,10 +102,12 @@ def get_albu_transform(transform, config):
                     A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, p=0.5),
                 ]),
                 
-                # Noise/扭曲/Mask
-                A.ISONoise(p=0.3),
-                A.OpticalDistortion(p=0.2),
-                A.Cutout(num_holes=4, max_h_size=90, max_w_size=120, fill_value=0, always_apply=False, p=0.3),
+                # 扭曲/Noise/Mask
+                A.OpticalDistortion(p=0.3),
+                A.OneOf([
+                    A.ISONoise(p=0.5),
+                    A.Cutout(num_holes=4, max_h_size=90, max_w_size=120, fill_value=0, p=0.5),
+                ]),
 
                 # 归一化
                 A.Resize(config.image_size,config.image_size),
