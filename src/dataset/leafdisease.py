548a14d9f7ecce8cd140db7ec70c550420fc3e7d
hijkzzz <janhu9527@gmail.com>
2021-02-04 09:10:31 +0000

    update

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -91,13 +91,20 @@ def get_albu_transform(transform, config):
     elif transform == "strong_fix2":
         train_trans =  A.Compose([
                 A.Compose([
-                    # 旋转平移
+                    # 旋转
                     A.RandomRotate90(p=0.5),
                     A.Flip(p=0.5),
 
+                    # 缩放
+                    A.OneOf([
+                        A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
+                        A.RandomResizedCrop(width=config.image_size, height=config.image_size,
+                            scale=(0.2, 1.0), ratio=(0.75, 1.33), p=0.2),
+                    ], p=0.3),
+
                     # 光照
                     A.OneOf([
-                        A.RandomSunFlare(num_flare_circles_lower=1, num_flare_circles_upper=2, src_radius=200, p=0.2),
+                        A.RandomSunFlare(num_flare_circles_lower=1, num_flare_circles_upper=2, src_radius=150, p=0.2),
                         A.RandomShadow(p=0.2),
                     ], p=0.3),
 
@@ -107,18 +114,11 @@ def get_albu_transform(transform, config):
                         A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, p=0.2),
                     ], p=0.5),
                     
-                    # Dropout
+                    # prevent overfit
                     A.OneOf([
                         A.OpticalDistortion(p=0.2),
                         A.Cutout(num_holes=3, max_h_size=200, max_w_size=200, fill_value=0, p=0.2)
                     ], p=0.5),
-
-                    # 缩放
-                    A.OneOf([
-                        A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
-                        A.RandomResizedCrop(width=config.image_size, height=config.image_size,
-                            scale=(0.2, 1.0), ratio=(0.75, 1.33), p=0.2),
-                    ], p=0.3)
                 ], p=config.p),
 
                 # 归一化
