fdf4a916cb58d2bd05537b8e3989766ae630d98c
sy <772922440@qq.com>
2021-02-08 11:00:07 +0800

    add complex clf

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -7,6 +7,8 @@ from .resnet import *
 def get_backbone(backbone, config):
     if config.forward_features:
         backbone = TimmBackboneFeature(model_name=backbone, target_size=config.target_size)
+    elif config.addclf:
+        backbone = TimmBackboneAddClf(model_name=backbone, target_size=config.target_size)
     else:
         backbone = TimmBackbone(model_name=backbone, target_size=config.target_size)
     return backbone
--- src/model/resnet.py
+++ src/model/resnet.py
@@ -8,7 +8,6 @@ class TimmBackbone(nn.Module):
     def __init__(self, model_name= 'mobilenetv3_large_100', pretrained=True, target_size = 5):
         super().__init__()
         self.model = timm.create_model(model_name, pretrained=pretrained)
-        # n_features = self.model.fc.in_features
         self.model.reset_classifier(target_size)
 
     def forward(self, x):
@@ -20,7 +19,6 @@ class TimmBackboneFeature(nn.Module):
     def __init__(self, model_name= 'tf_efficientnet_b3', pretrained=True, target_size = 5):
         super().__init__()
         self.model = timm.create_model(model_name, pretrained=pretrained)
-        # n_features = self.model.fc.in_features
         self.model.reset_classifier(target_size)
 
     def forward(self, x):
@@ -29,3 +27,34 @@ class TimmBackboneFeature(nn.Module):
         if self.model.drop_rate > 0.:
             xx = F.dropout(xx, p=self.model.drop_rate, training=self.model.training)
         return self.model.classifier(xx), xx
+
+
+
+class TimmBackboneAddClf(nn.Module):
+    def __init__(self, model_name= 'mobilenetv3_large_100', pretrained=True, target_size = 5):
+        super().__init__()
+
+        self.dim_m1 = 512
+        self.dim_m2 = 256
+
+
+        self.model = timm.create_model(model_name, pretrained=pretrained)
+        self.clf = nn.Sequential(
+            nn.Linear(1000, self.dim_m1),
+            nn.ReLU(),
+            nn.BatchNorm1d(self.dim_m1),
+            nn.Dropout(p=0.3),
+            
+            nn.Linear(self.dim_m1, self.dim_m2),
+            nn.ReLU(),
+            nn.BatchNorm1d(self.dim_m2),
+            nn.Dropout(p=0.3),
+
+            nn.Linear(self.dim_m2, target_size),
+            
+            )
+
+    def forward(self, x):
+        x = self.model(x)
+        x = self.clf(x)
+        return x
