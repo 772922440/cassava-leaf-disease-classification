9831dfe4ce343212ac57dfd1616a1cc2daa991c2
hujian <janhu9527@gmail.com>
2021-02-10 21:41:05 +0800

    add five aug

--- src/dataset/leafdisease.py
+++ src/dataset/leafdisease.py
@@ -9,6 +9,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import torchvision
 import PIL
+import timm
 
 try:
     # from pytorch_toolbelt.inference import tta
@@ -31,46 +32,13 @@ def get_albu_transform(transform, config):
     ])
     
     # train
-    if transform == "strong":
-        train_trans = A.Compose([
-                A.Compose([
-                    A.RandomRotate90(),
-                    A.Flip(),
-                    A.RandomResizedCrop(width=config.image_size, height=config.image_size, scale=(0.5, 1.0)),
-                    A.HorizontalFlip(p=0.5),
-                    A.RandomBrightnessContrast(p=0.2),
-                   A.OneOf([
-                       A.MotionBlur(p=.2),
-                       A.MedianBlur(blur_limit=3, p=0.1),
-                       A.Blur(blur_limit=3, p=0.1),
-                       ], p=0.2),
-                   A.OneOf([
-                       A.OpticalDistortion(p=0.3),
-                       A.GridDistortion(p=.1),
-                       A.IAAPiecewiseAffine(p=0.3),
-                       ], p=0.2),
-                    A.OneOf([
-                        A.CLAHE(clip_limit=2),
-                        A.IAASharpen(),
-                      A.IAAEmboss(),
-                        A.RandomBrightnessContrast(),            
-                        ], p=0.3),
-                   A.HueSaturationValue(p=0.3),
-                    ], p=config.p),
-                A.Resize(config.image_size,config.image_size),
-                A.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                ),
-                ToTensorV2(),
-                ])
-    elif transform == "strong_fix":
+    if transform == "strong_fix":
         train_trans =  A.Compose([
                 A.Compose([
                     A.RandomRotate90(p=0.5),
                     A.Flip(p=0.5),
-                    A.RandomResizedCrop(width=config.image_size, height=config.image_size, scale=(0.5, 1.0), p=0.5),
-                    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.5, rotate_limit=45, p=0.2),
+                    A.RandomResizedCrop(width=config.image_size, height=config.image_size, scale=(0.2, 1.0), p=0.5),
+                    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
                     A.IAAAdditiveGaussianNoise(p=0.2),
                     A.OneOf([
                         A.CLAHE(clip_limit=2),
@@ -90,11 +58,9 @@ def get_albu_transform(transform, config):
                 A.Compose([
                     A.RandomRotate90(p=0.5),
                     A.Flip(p=0.5),
-                    A.RandomResizedCrop(width=config.image_size, height=config.image_size, scale=(0.5, 1.0), p=0.5),
-                    A.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1, p=0.5),
+                    A.RandomResizedCrop(width=config.image_size, height=config.image_size, scale=(0.2, 1.0), p=0.5),
+                    A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, p=0.5),
                 ], p=config.p),
-
-                # 归一化
                 A.Resize(config.image_size,config.image_size),
                 A.Normalize(
                     mean=[0.485, 0.456, 0.406],
@@ -102,7 +68,7 @@ def get_albu_transform(transform, config):
                 ),
                 ToTensorV2(),
                 ])
-    elif transform == "strong_fix3": 
+    elif transform == "strong_fix3" or transform == "light": 
         train_trans =  A.Compose([
                 A.Transpose(p=0.5),
                 A.HorizontalFlip(p=0.5),
@@ -126,64 +92,61 @@ def get_albu_transform(transform, config):
                 ),
                 ToTensorV2(),
                 ])
-
-        test_trans = A.Compose([
+    elif transform == "heavy": 
+        train_trans =  A.Compose([
             A.Resize(config.image_size, config.image_size),
+            A.Transpose(p=0.5),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.5),
+            A.HueSaturationValue(
+                hue_shift_limit=0.2, 
+                sat_shift_limit=0.2, 
+                val_shift_limit=0.2, 
+                p=0.5
+            ),
+            A.RandomBrightnessContrast(
+                brightness_limit=(-0.1,0.1), 
+                contrast_limit=(-0.1, 0.1), 
+                p=0.5
+            ),
+            A.RGBShift(r_shift_limit=20, 
+                g_shift_limit=20, 
+                b_shift_limit=20, 
+                always_apply=False, 
+                p=0.5
+            ),
+            A.ChannelDropout(
+                channel_drop_range=(1, 1), 
+                fill_value=0, 
+                always_apply=False, 
+                p=0.5
+            ),
+            A.GridDistortion(num_steps=5, 
+                distort_limit=0.3, 
+                interpolation=1, 
+                border_mode=4, 
+                value=None, 
+                mask_value=None, 
+                always_apply=False, 
+                p=0.5
+            ),
+            A.CoarseDropout(p=0.5),
+            A.Cutout(p=0.5),
             A.Normalize(
                 mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225],
             ),
             ToTensorV2(),
-        ])       
-        # 外加 cutmix
-    elif transform == "valid_tta":
-        train_trans =  A.Compose([
-                    A.Compose([
-                    # 旋转平移
-                        A.RandomRotate90(p=0.3),
-                        A.Flip(p=0.3),
-                        A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.3),
-                        A.CenterCrop(height = config.image_size, width = config.image_size, p=0.5),
-
-                        # 光照
-                        A.OneOf([
-                            A.RandomSunFlare(num_flare_circles_lower=1, num_flare_circles_upper=2, src_radius=200, p=0.2),
-                            A.RandomShadow(p=0.2),
-                        ], p=0.3),
-
-                        # 色彩
-                        A.OneOf([
-                            A.RandomBrightnessContrast(p=0.2),
-                            A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2, p=0.2),
-                        ], p=0.3),
-                        
-                        # 扭曲/Mask
-                        A.OneOf([
-                            A.OpticalDistortion(p=0.2),
-                            A.Cutout(num_holes=3, max_h_size=200, max_w_size=200, fill_value=0, p=0.2)
-                        ], p=0.3),
-                    ], p=config.p),
-
-                # 归一化
-
-                A.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                ),
-                A.Resize(config.image_size ,config.image_size),
-                ToTensorV2(),
-                ])
-
-        test_trans = tta.Compose([
-                        tta.HorizontalFlip(),
-                        tta.VerticalFlip(),
-                        tta.FiveCrops(crop_height = config.image_size * 3 //4, crop_width = config.image_size * 3 //4)
-                    ])
-
+            ])
+    elif transform == "auto_augment_transform": 
+        train_trans =  timm.data.auto_augment.auto_augment_transform('originalr', None)
     else:
         raise "transform error"
 
     return train_trans, test_trans
+
+
 
 
 def get_torch_transform(image_size):
