e0faffb501bcc1f2cd57d6fc9da35b1a1dffcfc4
hijkzzz <janhu9527@gmail.com>
2021-02-13 04:29:24 +0000

    update

--- src/ensemble_train.py
+++ src/ensemble_train.py
@@ -244,7 +244,7 @@ def main(local_rank=0, world_size=1):
         return 1
 
     # output dir
-    model_save_path = join(config.model_base_path, config.backbone + config.model_suffix)
+    model_save_path = join(config.model_base_path, config.backbone + str(config.model_suffix))
     utils.mkdir(model_save_path)
 
     # init dist
